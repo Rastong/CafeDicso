9581c8fed10c21af5e15f36c500befb7df0470ff
pietryb <brandon.pietryka@gmail.com>
2021-10-15 16:11:59 -0400

    menu item first commit

---
+++ CafeDisco/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeDisco
+{
+    class Item
+    {
+        public string Name;
+        public double Price;
+        
+
+        public Item(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ CafeDisco/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeDisco
+{
+    class Menu
+    {
+        //property
+        public List<Item> DrinkMenu;
+        public List<Item> FoodMenu;
+
+        //constructor
+        public Menu()
+        {
+            DrinkMenu = new List<Item>
+            {
+                new Item("Chai Latte", 3.25),
+                new Item("Steamer", 2.75),
+                new Item("Iced Coffee", 1.99),
+                new Item("BullShot", 4.29),
+                new Item("Cuban Coffee", 2.99),
+                new Item("Cafe con leche", 2.59),
+                new Item("Nitro Cold Brew", 4.79),
+                new Item("Lavendar Latte", 4.50),
+                new Item("Chappuccino", 3.95),
+                new Item("Reese Mocha", 4.95),
+                new Item("Peppermint Patty", 4.95),
+                new Item("Americano", 2.85),
+
+
+            };
+
+            FoodMenu = new List<Item>
+            {
+                new Item("Bagel", 3.99),
+                new Item("Corn Dog", 1.99),
+                new Item("Parmesan Leek Pastry", 4.25),
+                new Item("The Godfather", 6.69),
+            };
+            
+        }
+
+
+    }
+}
