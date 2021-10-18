9f1a6a700edd15396a528ea39c4128d187465efe
scott <scottjthayer@gmail.com>
2021-10-18 16:01:50 -0400

    added new loop

--- CafeDisco/Program.cs
+++ CafeDisco/Program.cs
@@ -6,66 +6,79 @@ namespace CafeDisco
     {
         static void Main(string[] args)
         {
+            
             Menu menu = new Menu();
-            Cart myCart = new Cart();
-            Console.WriteLine("Welcome to Cafe Disco! Here is our menu.");
-            Console.WriteLine("DRINKS");
-            menu.DrinkMenu.ForEach(item => Console.WriteLine($"{item}"));
-            Console.WriteLine("\nFOOD");
-            menu.FoodMenu.ForEach(item => Console.WriteLine($"{item}"));       
-            while (true)
+            bool newCustomer = true;
+            while (newCustomer == true)
             {
-                //get main menu choice
-                int mainChoice = Options.GetMainChoice();
-                //add/remove choice
 
-                if (mainChoice == 1)
+                Cart myCart = new Cart();
+
+                Console.WriteLine("Welcome to Cafe Disco! Here is our menu.");
+                Console.WriteLine("DRINKS");
+                menu.DrinkMenu.ForEach(item => Console.WriteLine($"{item}"));
+                Console.WriteLine("\nFOOD");
+                menu.FoodMenu.ForEach(item => Console.WriteLine($"{item}"));
+                while (true)
                 {
-                    int addRemove = Options.AddOrRemove(menu);
+                    //get main menu choice
+                    int mainChoice = Options.GetMainChoice();
+                    //add/remove choice
 
-
-                    if (addRemove == 1)
+                    if (mainChoice == 1)
                     {
-                        //food/drink choice
-                        int foodOrDrink = Options.GetFoodOrDrink();
-                        int itemChoice = Options.GetItem(foodOrDrink, menu);
-                        myCart.AddToCart(foodOrDrink, itemChoice);
+                        int addRemove = Options.AddOrRemove(menu);
+
+
+                        if (addRemove == 1)
+                        {
+                            //food/drink choice
+                            int foodOrDrink = Options.GetFoodOrDrink();
+                            int itemChoice = Options.GetItem(foodOrDrink, menu);
+                            myCart.AddToCart(foodOrDrink, itemChoice);
+                        }
+                        else
+                        {
+                            myCart.RemoveFromCart();
+                        }
+
+                        Console.Write("Press any key to continue: ");
+                        Console.ReadKey();
+
+                        Console.Clear();
+
+                        Console.WriteLine("\nThanks! Would you like to: ");
                     }
-                    else
+
+                    else if (mainChoice == 2)
                     {
-                        myCart.RemoveFromCart();
+
+                        Console.Clear();
+                        myCart.PrintCart();
+                        Console.WriteLine($"\nThank you for shopping at Cafe Disco." +
+                            $"{"\nSubtotal: ",-35} ${myCart.GetSubTotal():0.00}" +
+                            $"{"\nTax: ",-35} ${myCart.GetTax():0.00}" +
+                            $"{"\nGrand Total: ",-35} ${myCart.GrandTotal():0.00}");
+                        break;
                     }
 
-                    Console.Write("Press any key to continue: ");
-                    Console.ReadKey();
+                    else if (mainChoice == 3)
+                    {
+                        Console.Clear();
+                        myCart.PrintCart();
+                        Console.WriteLine($"{"Your cart subtotal is",-35} ${myCart.GetSubTotal():0.00}");
+                    }
 
-                    Console.Clear();
+                    else if (mainChoice == 99)
+                    {
+                        menu.AddOrRemove();
+                    }
 
-                    Console.WriteLine("\nThanks! Would you like to: ");
-                }
-
-                else if (mainChoice == 2)
-                {
-
-                    Console.Clear();
-                    myCart.PrintCart();
-                    Console.WriteLine($"\nThank you for shopping at Cafe Disco." + 
-                        $"{"\nSubtotal: ",-35} ${myCart.GetSubTotal():0.00}" + 
-                        $"{"\nTax: ",-35} ${myCart.GetTax():0.00}" + 
-                        $"{"\nGrand Total: ",-35} ${myCart.GrandTotal():0.00}");
-                    break;
-                }
-
-                else if (mainChoice == 3)
-                {
-                    Console.Clear();
-                    myCart.PrintCart();
-                    Console.WriteLine($"{"Your cart subtotal is", -35} ${myCart.GetSubTotal():0.00}");
-                }
-
-                else if (mainChoice == 99)
-                {
-                    menu.AddOrRemove();
+                    else if (mainChoice == 96)
+                    {
+                        Console.WriteLine("Shutting down PoS terminal. Great job today! beep boop.");
+                        Environment.Exit(0);
+                    }
                 }
             }
         }
